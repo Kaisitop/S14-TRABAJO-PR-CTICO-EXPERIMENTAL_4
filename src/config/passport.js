d142c1c56209c81b4a971c2a328b14f33d16074b
Kaisitop <jandinoc@unemi.edu.ec>
2025-11-23 18:50:43 -0500

    add cahnges

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,10 +2,14 @@ import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import OAuthUser from '../models/OAuthUser.js';
 
+const callbackURL = process.env.NODE_ENV === "production"
+  ? "https://pe4.onrender.com/auth/google/callback"
+  : "http://localhost:3000/auth/google/callback";
+
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: callbackURL
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await OAuthUser.findOne({ googleId: profile.id });
